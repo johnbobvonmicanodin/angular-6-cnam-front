4eca94badf5e25d5199ae3ecaa5751fbe87619c2
Mathieu Comandini <math.comandini@outlook.fr>
2018-12-11 14:03:55 +0100

    premier commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { routerTransition } from '../router.animations';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    animations: [routerTransition()]
+})
+export class LoginComponent implements OnInit {
+    constructor(
+        private translate: TranslateService,
+        public router: Router
+        ) {
+            this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
+            this.translate.setDefaultLang('en');
+            const browserLang = this.translate.getBrowserLang();
+            this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
+    }
+
+    ngOnInit() {}
+
+    onLoggedin() {
+        localStorage.setItem('isLoggedin', 'true');
+    }
+}
